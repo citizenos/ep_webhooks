9f33c5d61cea8023c68c516f5798e18e98487a5f
ilmar <ilmar.tyrk@gmail.com>
2020-12-02 00:09:57 +0200

    Update code

--- serverHooks.js
+++ serverHooks.js
@@ -1,13 +1,14 @@
-var PLUGIN_NAME = 'ep_webhooks';
+'use strict';
 
-var _ = require('lodash');
-var request = require('superagent');
+const PLUGIN_NAME = 'ep_webhooks';
+const _ = require('lodash');
+const request = require('superagent');
 
-var logger = require('ep_etherpad-lite/node_modules/log4js').getLogger(PLUGIN_NAME);
-var padMessageHandler = require('ep_etherpad-lite/node/handler/PadMessageHandler.js');
+const logger = require('ep_etherpad-lite/node_modules/log4js').getLogger(PLUGIN_NAME);
+const padMessageHandler = require('ep_etherpad-lite/node/handler/PadMessageHandler.js');
 
-var pluginSettings; // set with loadSettings hook
-var changedPads = {}; // Pads that have changed. key = padId, value = 1
+let pluginSettings; // set with loadSettings hook
+let changedPads = {}; // Pads that have changed. key = padId, value = 1
 
 /**
  * Call the webhooks
@@ -16,41 +17,44 @@ var changedPads = {}; // Pads that have changed. key = padId, value = 1
  *
  * @see {@link https://lodash.com/docs#debounce}
  */
-var callPadUpdateWebhooks = _.debounce(function () {
-    logger.debug('callPadUpdateWebhooks', changedPads);
+const callPadUpdateWebhooks = _.debounce(() => {
+  logger.debug('callPadUpdateWebhooks', changedPads);
 
-    // No pads changed
-    if (!Object.keys(changedPads).length) {
-        return;
-    }
+  // No pads changed
+  if (!Object.keys(changedPads).length) {
+    return;
+  }
 
-    var changedPadIds = Object.keys(changedPads);
-    changedPads = {};
+  const changedPadIds = Object.keys(changedPads);
+  changedPads = {};
 
-    var updateHooksToCall = _.get(pluginSettings, ['pads', 'update']);
+  const updateHooksToCall = _.get(pluginSettings, ['pads', 'update']);
 
-    if (Array.isArray(updateHooksToCall)) {
-        // Fire and forget - no guarantees of delivery for now
-        updateHooksToCall.forEach(function (path) {
-            var req = request
-                .post(path);
+  if (Array.isArray(updateHooksToCall)) {
+    // Fire and forget - no guarantees of delivery for now
+    updateHooksToCall.forEach((path) => {
+      const req = request
+          .post(path);
 
-            // The support for self signed certificates
-            var caCert = pluginSettings.caCert;
-            if (caCert) {
-                req.ca(caCert);
+      // The support for self signed certificates
+      const caCert = pluginSettings.caCert;
+      if (caCert) {
+        req.ca(caCert);
+      }
+
+      req
+          .set('X-API-KEY', pluginSettings.apiKey)
+          .send({padIds: changedPadIds})
+          .end((err, res) => {
+            if (err) {
+              logger.error('callPadUpdateWebhooks - HTTP POST failed to ',
+                  path,
+                  '. Error was',
+                  err);
             }
-
-            req
-                .set('X-API-KEY', pluginSettings.apiKey)
-                .send({padIds: changedPadIds})
-                .end(function (err, res) {
-                    if (err) {
-                        logger.error('callPadUpdateWebhooks - HTTP POST failed to ', path, '. Error was', err);
-                    }
-                });
-        });
-    }
+          });
+    });
+  }
 }, 1000, {maxWait: 5000});
 
 /**
@@ -62,25 +66,25 @@ var callPadUpdateWebhooks = _.debounce(function () {
  *
  * @see {@link http://etherpad.org/doc/v1.5.7/#index_handlemessage}
  */
-exports.handleMessage = function (hook_name, context, cb) {
-    logger.debug('handleMessage');
+exports.handleMessage = (hook, context, cb) => {
+  logger.debug('handleMessage');
 
-    if (pluginSettings) {
-        var messageType = _.get(context.message, 'data.type');
+  if (pluginSettings) {
+    const messageType = _.get(context.message, 'data.type');
 
-        if (messageType === 'USER_CHANGES') {
-            var padId = _.get(padMessageHandler.sessioninfos[context.client.id], 'padId');
-            if (padId) {
-                logger.debug('handleMessage', 'PAD CHANGED', padId);
-                changedPads[padId] = 1; // Use object, as then I don't need to worry about duplicates
-                callPadUpdateWebhooks();
-            } else {
-                logger.warn('handleMessage', 'Pad changed, but no padId!');
-            }
-        }
+    if (messageType === 'USER_CHANGES') {
+      const padId = _.get(padMessageHandler.sessioninfos[context.client.id], 'padId');
+      if (padId) {
+        logger.debug('handleMessage', 'PAD CHANGED', padId);
+        changedPads[padId] = 1; // Use object, as then I don't need to worry about duplicates
+        callPadUpdateWebhooks();
+      } else {
+        logger.warn('handleMessage', 'Pad changed, but no padId!');
+      }
     }
+  }
 
-    return cb([context.message]);
+  return cb([context.message]);
 };
 
 /**
@@ -91,21 +95,25 @@ exports.handleMessage = function (hook_name, context, cb) {
  *
  * @see {@link http://etherpad.org/doc/v1.5.7/#index_loadsettings}
  */
-exports.loadSettings = function (hook_name, args) {
-    var settings = args.settings;
-    if (settings && settings[PLUGIN_NAME]) {
-        pluginSettings = settings[PLUGIN_NAME];
+exports.loadSettings = (hook, args, cb) => {
+  const settings = args.settings;
+  if (settings && settings[PLUGIN_NAME]) {
+    pluginSettings = settings[PLUGIN_NAME];
 
-        logger.debug('loadSettings', 'pluginSettings', pluginSettings);
+    logger.debug('loadSettings', 'pluginSettings', pluginSettings);
 
-        var caCert = pluginSettings.caCert;
-        if (caCert) {
-            if (caCert.indexOf('-----BEGIN CERTIFICATE-----') !== 0) {
-                logger.error('Invalid configuration! If you provide caCert, make sure it looks like a cert.');
-                process.exit(1);
-            }
-        }
-    } else {
-        logger.warn('Plugin configuration not found, doing nothing.')
+    const caCert = pluginSettings.caCert;
+    if (caCert) {
+      if (caCert.indexOf('-----BEGIN CERTIFICATE-----') !== 0) {
+        const message = `Invalid configuration! If you provide caCert,
+        make sure it looks like a cert.`;
+        logger.error(message);
+        throw new Error(message);
+      }
     }
+  } else {
+    logger.warn('Plugin configuration not found, doing nothing.');
+  }
+
+  return cb();
 };
